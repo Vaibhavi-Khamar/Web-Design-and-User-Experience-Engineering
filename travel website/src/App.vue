5c673a905ff98f247f02edbccdf85188adea0c7b
Vaibhavi Khamar <44368227+Vaibhavi-Khamar@users.noreply.github.com>
2020-07-13 19:07:49 -0400

    travel website using bootstrap

---
+++ travel website/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+    <!-- <BootstrapVue /> -->
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  name: "App",
+  components: {
+    Navbar
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: white;
+}
+</style>
